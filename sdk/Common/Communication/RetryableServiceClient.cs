cb33a39c3c3add6c6d8ab43aa8113dd053930bb9
shaoqiang.jingsq <shaoqiang.jingsq@alibaba-inc.com>
2015-12-15 10:31:50 +0800

    modify retry time

--- sdk/Common/Communication/RetryableServiceClient.cs
+++ sdk/Common/Communication/RetryableServiceClient.cs
@@ -56,7 +56,7 @@ namespace Aliyun.OSS.Common.Communication
 
         public ServiceResponse Send(ServiceRequest request, ExecutionContext context)
         {
-            return SendImpl(request, context, MaxRetryTimes);
+            return SendImpl(request, context, 0);
         }
 
         private ServiceResponse SendImpl(ServiceRequest request, ExecutionContext context, int retryTimes)
