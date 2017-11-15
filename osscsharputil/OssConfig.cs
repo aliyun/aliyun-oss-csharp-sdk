c26333409b5c48d88d9dc75338762ede35718ba8
qi.xu <qi.xu@alibaba-inc.com>
2017-11-15 11:20:08 -0800

    osscsharputil

---
+++ osscsharputil/OssConfig.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace osstestcsharp
+{
+    public class OssConfig
+    {
+        public static string Version = "oss-perf-test-1.0.0 ";
+        public static string Endpoint = "";
+        public static string AccessKeyId = "";
+        public static string AccessKeySecret = "";
+        public static string BucketName = "";
+        public static string OssCfgFile = "oss.ini";
+        
+        public static string Command = "";
+        public static string BaseLocalFile = "";
+        public static string BaseRemoteKey = "";
+
+        public static int PartSize = 100*1024*1024;
+        public static int Parallel = 4;
+        public static int Multithread = 0;
+        public static int LoopTimes = 1;
+        public static bool Persistent = false;
+        public static bool DifferentSource = false;
+        public static bool CrcCheck = true;
+
+        public static string HelpExampleString = "";
+
+        public static void config(string[] args)
+        {
+            parseCmdArgs(args); 
+            parseCfgFile();
+            printCfgInfo();
+        }
+
+        public static void parseCmdArgs(string[] args)
+        {
+            if (args.Length == 0)
+                printHelp();
+            CmdLineArgParser cmdParser = new CmdLineArgParser(args);
+            cmdParser.Parse(); 
+        }
+
+        public static void parseCfgFile() 
+        {
+            if (!File.Exists(OssConfig.OssCfgFile)) 
+            {
+                FileStream fs = new FileStream(OssConfig.OssCfgFile, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                StreamWriter sw = new StreamWriter(fs);
+
+                sw.Write("Endpoint=\nAccessKeyId=\nAccessKeySecret=");
+                sw.Flush();
+                sw.Close();
+                fs.Close(); 
+            }
+
+            StreamReader sr = new StreamReader(OssConfig.OssCfgFile, Encoding.Default); 
+            String line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                line = line.Trim();
+                String[] arrKeyValue;
+                if (line.StartsWith("Endpoint="))
+                {
+                    arrKeyValue = line.Split('=');
+                    OssConfig.Endpoint = arrKeyValue[1]; 
+                }
+                else if (line.StartsWith("AccessKeyId="))
+                {
+                    arrKeyValue = line.Split('=');
+                    OssConfig.AccessKeyId = arrKeyValue[1]; 
+                }
+                else if (line.StartsWith("AccessKeySecret="))
+                {
+                    arrKeyValue = line.Split('=');
+                    OssConfig.AccessKeySecret = arrKeyValue[1];
+                }
+                else if (line.StartsWith("BucketName="))
+                {
+                    arrKeyValue = line.Split('=');
+                    OssConfig.BucketName = arrKeyValue[1];
+                }
+            }
+            sr.Close();
+
+            if (OssConfig.Endpoint == "" || OssConfig.BucketName=="")
+            {
+                Console.WriteLine("Error Configuration : Endpoint or BucketName is empty . ");
+                Console.WriteLine("Please check the config file {0} in the directory {1} . ", OssConfig.OssCfgFile, Environment.CurrentDirectory);
+                Environment.Exit(0);
+            }
+        }
+
+        public static void printCfgInfo()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("version         : " + OssConfig.Version);
+            Console.WriteLine("endpoint        : " + OssConfig.Endpoint);
+            Console.WriteLine("accessKeyId     : " + OssConfig.AccessKeyId);
+            Console.WriteLine("bucketName      : " + OssConfig.BucketName);
+            Console.WriteLine("command         : " + OssConfig.Command);
+            Console.WriteLine("localfile       : " + OssConfig.BaseLocalFile);
+            Console.WriteLine("remotekey       : " + OssConfig.BaseRemoteKey);
+            if (OssConfig.Command == "upload_resumable")
+            {
+                Console.WriteLine("parallel        : " + OssConfig.Parallel);
+                Console.WriteLine("partsize        : " + OssConfig.PartSize);
+            }
+            Console.WriteLine("multithread     : " + OssConfig.Multithread);
+            Console.WriteLine("looptimes       : " + OssConfig.LoopTimes);
+            Console.WriteLine("persistent      : " + OssConfig.Persistent);
+            Console.WriteLine("differentsource : " + OssConfig.DifferentSource);
+        }
+
+        public static void printHelp()
+        {
+            string strHelp = "\n";
+            strHelp += "Usage: mono osstestcsharp.exe  [-h] [-v] [-c COMMAND] [-f LOCALFILE]      \n";
+            strHelp += "                               [-k REMOTEKEY] [-p PARALLEL]      \n";
+            strHelp += "                               [-m MULTITHREAD] [-partsize PARTSIZE]      \n";
+            strHelp += "                               [-loop LOOPTIMES] [--persistent]      \n";
+            strHelp += "                               [--differentsource]      \n";
+            strHelp += "Optional arguments:      \n";
+            strHelp += "  -h, --help          show this help message and exit.           \n";
+            strHelp += "  -v                  show program's version number and exit.    \n";
+            strHelp += "  -c COMMAND          Command Type : upload, upload_resumable, upload_async, download .  \n";
+            strHelp += "  -f LOCALFILE        local filename to transfer.                \n";
+            strHelp += "  -k REMOTEKEY        remote object key.                         \n";
+            strHelp += "  -partsize PARTSIZE  part size parameter for resume breakpoint transfer.                \n";
+            strHelp += "  -p PARALLEL         parallel threads parameter for resumable breakpoint transfer.      \n";
+            strHelp += "  -m MULTITHREAD      multithread number for transfer.           \n";
+            strHelp += "  -loop LOOPTIMES     loop times for transfer.                   \n";
+            strHelp += "  --persistent        Whether run the command persistantly.      \n";
+            strHelp += "  --differentsource   Whether transfer from different source files.  \n";
+
+            string strExample = "\nExamples :  \n";
+            strExample += "    mono osstestcsharp.exe -c upload -f mylocalfilename -k myobjectkeyname \n";
+            strExample += "    mono osstestcsharp.exe -c upload_resumable -f mylocalfilename -k myobjectkeyname -p 5 \n";
+            strExample += "    mono osstestcsharp.exe -c upload_async -f mylocalfilename -k myobjectkeyname \n";
+            strExample += "    mono osstestcsharp.exe -c upload -f mylocalfilename -k myobjectkeyname -m 5 \n";
+            strExample += "    mono osstestcsharp.exe -c download -f mylocalfilename -k myobjectkeyname \n";
+            strExample += "    mono osstestcsharp.exe -c download -f mylocalfilename -k myobjectkeyname -m 5 \n";
+
+            OssConfig.HelpExampleString = strHelp + strExample;
+            Console.WriteLine(OssConfig.HelpExampleString);
+            Environment.Exit(0);
+        }
+
+        public static void main(string[] args)
+        {
+            OssConfig.config(args);
+        }
+    }
+
+
+    class CmdLineArgParser
+    {
+        public static ArrayList alArgs = new ArrayList();
+
+        public CmdLineArgParser(string[] args)
+        {
+            for (int i = 0; i<args.Length; i++)
+            {
+                alArgs.Add(args[i]);
+            }
+        }
+
+        public string getArgValueByName(string argumentName)
+        {
+            int iPos = alArgs.IndexOf(argumentName);
+            return alArgs[iPos + 1].ToString();
+        }
+
+        public bool has(string argumentName)
+        {
+            return (alArgs.IndexOf(argumentName) >= 0);
+        }
+
+        public bool Parse()
+        {
+            if (CmdLineArgParser.alArgs.Count==0)
+            {
+                OssConfig.printHelp();                
+            }
+
+            if (this.has("-h") || this.has("help"))
+            {
+                OssConfig.printHelp();
+            }
+            if (this.has("-v") || this.has("--version"))
+            {
+                Console.WriteLine(OssConfig.Version);
+                Environment.Exit(0);
+            }
+            if (this.has("-c"))
+            {
+                OssConfig.Command = this.getArgValueByName("-c");
+                if (OssConfig.Command == "up") 
+                {
+                    OssConfig.Command = "upload";
+                }
+                else if (OssConfig.Command == "upr")
+                {
+                    OssConfig.Command = "upload_resumable";
+                }
+                else if (OssConfig.Command == "upa")
+                {
+                    OssConfig.Command = "upload_async";
+                }
+                else if (OssConfig.Command == "dn")
+                {
+                    OssConfig.Command = "download";
+                }
+                else if (OssConfig.Command == "dnr")
+                {
+                    OssConfig.Command = "download_resumable";
+                }
+            }
+            else
+            {
+                OssConfig.printHelp();
+            }
+            if (this.has("-f"))
+            {
+                OssConfig.BaseLocalFile = this.getArgValueByName("-f");
+            }
+            if (this.has("-k"))
+            {
+                OssConfig.BaseRemoteKey = this.getArgValueByName("-k");
+            }
+            if (this.has("-p"))
+            {
+                OssConfig.Parallel = Convert.ToInt32(this.getArgValueByName("-p"));
+            }
+            if (this.has("-m"))
+            {
+                OssConfig.Multithread = Convert.ToInt32(this.getArgValueByName("-m"));
+            }
+            if (this.has("-partsize"))
+            {
+                OssConfig.PartSize = Convert.ToInt32(this.getArgValueByName("-partsize"));
+            }
+            if (this.has("-loop"))
+            {
+                OssConfig.LoopTimes = Convert.ToInt32(this.getArgValueByName("-looptimes"));
+            }
+            if (this.has("--persistent"))
+            {
+                OssConfig.Persistent = true;
+            }
+            if (this.has("--differentsource"))
+            {
+                OssConfig.DifferentSource = true;
+            }
+
+            return true;
+        }
+
+    }
+}
---
+++ osscsharputil/OssTestRunner.cs
@@ -0,0 +1,367 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Aliyun.OSS;
+using Aliyun.OSS.Common;
+using Aliyun.OSS.Util;
+
+namespace osstestcsharp
+{
+    public class OssTestRunner
+    {
+        public static double totalTransferSize = 0.000;
+        public static double totalTransferRate = 0.000;
+        public static double totalTransferDuration = 0.000;
+        private static DateTime totalStartTime;
+        private static DateTime totalStopTime;
+        public static string CallbackServer = "";
+        public static string CallbackBody = "bucket=${bucket}&object=${object}&etag=${etag}&size=${size}&mimeType=${mimeType}&" +
+                                     "my_var1=${x:var1}&my_var2=${x:var2}";
+        public AutoResetEvent _event = new AutoResetEvent(false);
+        public OssClient ossClient = null;
+        public string taskID = "0";
+        public string strLocalFile = "";
+        public string strRemoteKey = "";
+        private DateTime dtStartTime;
+        private DateTime dtStopTime;
+        private double transferSize;
+        private double transferDuration;
+        private double transferRate;
+
+        
+        public OssTestRunner()
+        {
+            ossClient = new OssClient(OssConfig.Endpoint, OssConfig.AccessKeyId, OssConfig.AccessKeySecret);
+        }
+
+        ~OssTestRunner()
+        {
+        }
+        
+        public void runSingleTask(object obj)
+        {
+            if (OssConfig.LoopTimes == 0)
+            {
+                OssConfig.Persistent = true;
+            }
+
+            int runIndex = 1;
+            while ((runIndex <= OssConfig.LoopTimes) || OssConfig.Persistent)
+            {
+                switch (OssConfig.Command)
+                {
+                    case "upload":
+                        upload();
+                        break;
+                    case "upload_resumable":
+                        upload_resumble();
+                        break;
+                    case "upload_async":
+                        upload_async();
+                        break;
+                    case "download":
+                        download();
+                        break;
+                    default:
+                        Console.WriteLine("The Command Type Error : {0}", OssConfig.Command);
+                        OssConfig.printHelp();
+                        break;
+                }
+
+                runIndex++;
+            }
+
+            if (OssConfig.Multithread>=1)
+            {
+                ManualResetEvent mre = (ManualResetEvent)obj;
+                mre.Set();
+            }
+        }
+
+        public void upload()
+        {
+            transfer_start_initialize(); 
+
+            try
+            {
+                this.ossClient.PutObject(OssConfig.BucketName, this.strRemoteKey, this.strLocalFile);
+                Console.WriteLine("     Put object : {0} succeeded ! ", this.strRemoteKey);
+            }
+            catch (OssException ex)
+            {
+                Console.WriteLine("Failed with error code: {0}; Error info: {1}. \nRequestID:{2}\tHostID:{3}",
+                    ex.ErrorCode, ex.Message, ex.RequestId, ex.HostId);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed with error info: {0}", ex.ToString());
+            }
+
+            transfer_stop_statistic();
+        }
+
+        public void upload_resumble()
+        {
+            transfer_start_initialize(); 
+
+            try
+            {
+                var metadata = BuildCallbackMetadata(CallbackServer, CallbackBody);
+                var uploadResult = this.ossClient.ResumableUploadObject(OssConfig.BucketName, this.strRemoteKey, this.strLocalFile, metadata, null, OssConfig.PartSize);
+                var responseContent = GetCallbackResponse(uploadResult);
+                Console.WriteLine("Resumable upload object:{0} succeeded. ", this.strRemoteKey);
+            }
+            catch (OssException ex)
+            {
+                Console.WriteLine("Failed with error code: {0}; Error info: {1}. \nRequestID:{2}\tHostID:{3}",
+                    ex.ErrorCode, ex.Message, ex.RequestId, ex.HostId);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed with error info: {0}", ex.Message);
+            }
+
+            transfer_stop_statistic();
+        }
+
+        public void upload_async()
+        {
+            transfer_start_initialize();
+
+            try
+            {
+                using (var fs = File.Open(this.strLocalFile, FileMode.Open))
+                {
+                    var metadata = new ObjectMetadata();
+                    metadata.CacheControl = "No-Cache";
+                    metadata.ContentType = "text/html";
+                    this.ossClient.BeginPutObject(OssConfig.BucketName, this.strRemoteKey, fs, metadata, putObjectCallback, new string('a', 8));
+                    _event.WaitOne();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Put object failed, {0}", ex.Message);
+            }
+
+            transfer_stop_statistic();
+        }
+
+        public void download()
+        {
+            transfer_start_initialize(); 
+            DateTime dtStart = DateTime.Now;
+            try
+            {
+                var obj = this.ossClient.GetObject(OssConfig.BucketName, this.strRemoteKey);
+                using (var requestStream = obj.Content)
+                {
+                    byte[] buf = new byte[8192 * 4];
+                    var fs = File.Open(this.strLocalFile, FileMode.OpenOrCreate);
+                    var len = 0;
+                    var once_read_bytes = 8192* 4; //Default 8192 * 4 Bytes
+                    while ((len = requestStream.Read(buf, 0, once_read_bytes)) != 0)
+                    {
+                        fs.Write(buf, 0, len);
+                    }
+                    fs.Close();
+                }   
+                Console.WriteLine("Get object succeeded");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Get object failed. {0}", ex.Message);
+            }
+
+            transfer_stop_statistic();
+        }
+
+        public Stream download_async()
+        {
+            transfer_start_initialize();
+            DateTime dtStart = DateTime.Now;
+            try
+            {
+                var obj = this.ossClient.GetObject(OssConfig.BucketName, this.strRemoteKey);
+                return obj.Content;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Get object failed. {0}", ex.Message);
+            }
+
+            return null;
+        }
+        
+        private void putObjectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                var result = this.ossClient.EndPutObject(ar);
+                Console.WriteLine("ETag:{0}", result.ETag);
+                Console.WriteLine("User Parameter:{0}", ar.AsyncState as string);
+                Console.WriteLine("Put object succeeded");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Put object failed, {0}", ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("SN{0}## PutObjectCallback : Set AutoResetEvent ... ", this.taskID);
+                _event.Set();
+            }
+        }
+
+        private void transfer_start_initialize()
+        {
+            this.dtStartTime = DateTime.Now;
+            Console.WriteLine("\n++++ START {0} : taskID={1}, LocalFile={2}, RemoteKey={3}, StartTime={4} . ", OssConfig.Command, this.taskID, this.strLocalFile, this.strRemoteKey, this.dtStartTime.ToString("yyyy-MM-dd HH:mm:ss:ms")); 
+        }
+
+        private void transfer_stop_statistic()
+        {
+            this.dtStopTime = DateTime.Now;
+            TimeSpan ts = this.dtStopTime.Subtract(this.dtStartTime).Duration();
+            this.transferDuration = ts.TotalSeconds;
+            FileInfo fi = new FileInfo(this.strLocalFile);
+            this.transferSize = fi.Length/1024/1024; 
+            this.transferRate = this.transferSize/this.transferDuration; 
+            Console.WriteLine("\n---- STOP  {0} : taskID={1}, LocalFile={2}, RemoteKey={3}, StopTime={4} . ", 
+                                OssConfig.Command, this.taskID, this.strLocalFile, this.strRemoteKey, this.dtStopTime.ToString("yyyy-MM-dd HH:mm:ss:ms")); 
+            Console.WriteLine("#### STATISTIC : taskID={0}, TransferRate={1}MB/S, TransferSize={2}MB, TransferDuration={3}Seconds. ", 
+                                this.taskID, this.transferRate, this.transferSize, this.transferDuration);
+            OssTestRunner.totalTransferSize += this.transferSize; 
+        }
+
+        private static string GetCallbackResponse(PutObjectResult putObjectResult)
+        {
+            string callbackResponse = null;
+            using (var stream = putObjectResult.ResponseStream)
+            {
+                var buffer = new byte[4 * 1024];
+                var bytesRead = stream.Read(buffer, 0, buffer.Length);
+                callbackResponse = Encoding.Default.GetString(buffer, 0, bytesRead);
+            }
+            return callbackResponse;
+        }
+
+        private static ObjectMetadata BuildCallbackMetadata(string callbackUrl, string callbackBody)
+        {
+            string callbackHeaderBuilder = new CallbackHeaderBuilder(callbackUrl, callbackBody).Build();
+            string CallbackVariableHeaderBuilder = new CallbackVariableHeaderBuilder().
+                AddCallbackVariable("x:var1", "x:value1").AddCallbackVariable("x:var2", "x:value2").Build();
+
+            var metadata = new ObjectMetadata();
+            metadata.AddHeader(HttpHeaders.Callback, callbackHeaderBuilder);
+            metadata.AddHeader(HttpHeaders.CallbackVar, CallbackVariableHeaderBuilder);
+            return metadata;
+        }
+
+        public void setTaskID(int i)
+        {
+            this.taskID = i.ToString().PadLeft(2, '0');
+        }
+
+        public void setLocalFileAndRemoteKey()
+        {
+            this.strLocalFile = OssConfig.BaseLocalFile;
+            this.strRemoteKey = OssConfig.BaseRemoteKey;
+
+            if (OssConfig.Multithread >= 1)
+            {
+                if (OssConfig.Command.StartsWith("upload"))
+                {
+                    this.strRemoteKey += this.taskID;
+                    if (OssConfig.DifferentSource)
+                    {
+                        this.strLocalFile += this.taskID;
+                    }
+                }
+                else if (OssConfig.Command.StartsWith("download"))
+                {
+                    this.strLocalFile += this.taskID;
+                    if (OssConfig.DifferentSource)
+                    {
+                        this.strRemoteKey += this.taskID;
+                    }
+                }
+                else
+                {
+                    throw new Exception("The command type error : " + OssConfig.Command);
+                }
+            }
+
+            if ((OssConfig.Command.StartsWith("upload")) && (!File.Exists(this.strLocalFile)))
+            {
+                throw new Exception("Local file does not exist : " + this.strLocalFile);
+            }
+        }
+
+        public static void start_and_config(String[] args)
+        {
+            OssConfig.config(args);
+            OssTestRunner.totalStartTime = DateTime.Now;
+            Console.WriteLine("\nThe Begin : StartTime={0}", OssTestRunner.totalStartTime.ToString("yyyy-MM-dd HH:mm:ss:ms"));
+        }
+
+        public static void stop_and_statistic_report()
+        {
+            OssTestRunner.totalStopTime = DateTime.Now;
+            TimeSpan ts = OssTestRunner.totalStopTime.Subtract(OssTestRunner.totalStartTime).Duration();
+            OssTestRunner.totalTransferDuration = ts.TotalSeconds;
+            OssTestRunner.totalTransferRate = OssTestRunner.totalTransferSize / OssTestRunner.totalTransferDuration;
+            Console.WriteLine("\nThe End  : StopTime ={0}, Command={1}, LocalFile={2}, RemoteKey={3}, Parallel={4}, Multithread={5}. ", OssTestRunner.totalStopTime.ToString("yyyy-MM-dd HH:mm:ss:ms"), OssConfig.Command, OssConfig.BaseLocalFile, OssConfig.BaseRemoteKey, OssConfig.Parallel, OssConfig.Multithread);
+            Console.WriteLine("#### StatisticReport: AvgTransferRate={0}MB/S, TotalSize={1}MB, TotalDuration={2} Seconds.", OssTestRunner.totalTransferRate, OssTestRunner.totalTransferSize, OssTestRunner.totalTransferDuration);
+
+            Console.ReadKey();
+        }
+        
+        static void Main(string[] args)
+        {
+            start_and_config(args);
+
+            try
+            {
+                if (OssConfig.Multithread < 1)
+                {
+                    OssTestRunner ossTestRunner = new OssTestRunner();
+                    ossTestRunner.setLocalFileAndRemoteKey();
+                    ossTestRunner.runSingleTask("");
+                }
+                else
+                {
+                    ManualResetEvent[] mre = new ManualResetEvent[OssConfig.Multithread];
+                    for (int i = 1; i <= OssConfig.Multithread; i++)
+                    {
+                        OssTestRunner ossTestRunner = new OssTestRunner();
+                        ossTestRunner.setTaskID(i);
+                        ossTestRunner.setLocalFileAndRemoteKey();
+
+                        mre[i - 1] = new ManualResetEvent(false);
+                        ThreadPool.QueueUserWorkItem(new WaitCallback(ossTestRunner.runSingleTask), mre[i - 1]);
+                    }
+
+                    WaitHandle.WaitAll(mre);
+                }
+            }
+            catch (OssException ex)
+            {
+                Console.WriteLine("Failed with error code: {0}; Error info: {1}. \nRequestID:{2}\tHostID:{3}",
+                                 ex.ErrorCode, ex.Message, ex.RequestId, ex.HostId);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed with error info: {0}", ex.ToString());
+            }
+
+            stop_and_statistic_report();
+        }
+    }
+
+}
