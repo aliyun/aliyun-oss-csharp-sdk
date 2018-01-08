ef0de5803501841530ae5f9e92442fbf27defb9c
qi.xu <qi.xu@alibaba-inc.com>
2018-01-08 11:59:44 -0800

    update ConfigCore.cs

--- test/Util/ConfigCore.cs
+++ test/Util/ConfigCore.cs
@@ -6,20 +6,20 @@ namespace Aliyun.OSS.Test.Util
     // Please update the values of the following variables before running the test cases;
     public static class Config
     {
-        public static readonly string Endpoint = "10.101.200.203:8086";
-        public static readonly string AccessKeyId = "LTAIJPXxMLocA0fD";
-        public static readonly string AccessKeySecret = "l8SjZPosYFR8cHn7jR05SOUFd2u0T7";
-        public static readonly string ProxyHost = "";
-        public static readonly string ProxyPort = "";
+        public static readonly string Endpoint = "";  // your endpoint
+        public static readonly string AccessKeyId = ""; // your access key id
+        public static readonly string AccessKeySecret = ""; // your access key secret
+        public static readonly string ProxyHost = "";   // proxy host
+        public static readonly string ProxyPort = "3128";
         public static readonly string ProxyUser = "";
         public static readonly string ProxyPassword = "";
-        public static readonly string CallbackServer = "";
+        public static readonly string CallbackServer = "";  // callback server
         public static readonly string DisabledAccessKeyId = "";
         public static readonly string DisabledAccessKeySecret = "";
         public static readonly string SecondEndpoint = "";
-        public static readonly string UploadTestFile = "/Users/qi.xu/Downloads/wikiticker-index.s3.json";
-        public static readonly string MultiUploadTestFile = "/Users/qi.xu/Documents/存储产品-日志服务-v1.pptx";
-        public static readonly string ImageTestFile = "/Users/qi.xu/Downloads/self.jpg";
-        public static readonly string DownloadFolder = "/Users/qi.xu/Downloads/CSharp";
+        public static readonly string UploadTestFile = "";  // path to upload test file
+        public static readonly string MultiUploadTestFile = "";  // path to multi upload test file--it should be 1MB or bigger
+        public static readonly string ImageTestFile = ""; // path to upload test image file
+        public static readonly string DownloadFolder = ""; // path to download folder
     }
 }
